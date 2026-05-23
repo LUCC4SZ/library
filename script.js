9988700b15b0f0117fe5366afea45a0075c7157e
lucca-dev <luccazamora@gmail.com>
2026-05-23 14:58:14 -0300

    Add random and unique id generator logic

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(author, title, pages, read, id) {
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
 function addBook(author, title, pages, read) {
-    let book = new Book(author, title, pages, read);
+    let book = new Book(author, title, pages, read, crypto.randomUUID());
     myLibrary.push(book);
 }
 
 console.log(myLibrary);
 
 addBook("Tolkien", "The Lord of the Rings", 300, true);
+
